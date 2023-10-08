a92a1c5eb6de1473535e8f9ea23559d43e9d741d
miyunecadz <jvcadz@gmail.com>
2023-10-08 22:16:30 +0800

    Add BE-05 login user scaffolding

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Web\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,4 +28,8 @@ Route::get('/pages', function () {
 
 Route::get('/login', function () {
     return view('auth.login');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
 });
